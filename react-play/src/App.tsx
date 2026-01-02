b8b1f3e3fa653b99cf3bb07498d31290068b9788
takehiro1111 <t.soccer.0916@gmail.com>
2026-01-02 22:16:35 +0900

    feat: 記事一覧ページの作成

--- react-play/src/App.tsx
+++ react-play/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import { posts } from "./data/posts";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>記事一覧ページ</h1>
+        <ul>
+          {posts &&
+            posts.map((element) => {
+              return (
+                <li key={element.id}>
+                  <h2>{element.title}</h2>
+                  <a>{element.thumbnailUrl}</a>
+                  <p>{element.createdAt}</p>
+                  <p>{element.categories}</p>
+                  <p>{element.content}</p>
+                </li>
+              );
+            })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
