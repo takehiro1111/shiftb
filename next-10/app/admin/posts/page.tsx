96cc698e1d4c1dc1d32f70bfff26a95dfb15bfd3
takehiro1111 <t.soccer.0916@gmail.com>
2026-01-23 19:01:11 +0900

    refactor: posts from server component to client component

--- next-10/app/admin/posts/page.tsx
+++ next-10/app/admin/posts/page.tsx
@@ -1,28 +1,42 @@
-import { notFound } from "next/navigation";
+"use client";
+
+import { useEffect, useState } from "react";
 import { PostModel } from "@/app/generated/prisma/models/Post";
 import DisplayHeader from "@/app/admin/_components/DisplayHeader";
 import Link from "next/link";
 
-export default async function Page() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/admin/posts`, { cache: 'no-store' });
-  const { posts } = await res.json();
+export default function Page() {
+  const [posts, setPosts] = useState<PostModel[] | null>(null);
+  const [error, setError] = useState(false);
 
+  useEffect(() => {
+    const fetcher = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/admin/posts`);
+        const { posts } = await res.json();
+        setPosts(posts);
+      } catch (e) {
+        console.error(e);
+        setError(true);
+      }
+    };
+    fetcher();
+  }, []);
+
+  if (error) return <span>エラーが発生しました</span>;
   if (posts === null) return <span>Loading...</span>;
-  if (!posts) return notFound();
 
   return (
     <>
       <DisplayHeader title="記事一覧" entity="posts" />
       <div>
         <ul className="w-full">
-          {posts && posts.map((post: PostModel) => {
+          {posts.map((post: PostModel) => {
             return (
               <div key={post.id} className="py-4 border-b border-gray-400">
-                <>
-                  <Link href={`/admin/posts/${post.id}`} className="font-bold">
-                    {post.title}
-                  </Link>
-                </>
+                <Link href={`/admin/posts/${post.id}`} className="font-bold">
+                  {post.title}
+                </Link>
                 <p className="text-sm text-gray-500">
                   {new Date(post.updatedAt).toLocaleDateString()}
                 </p>
